229d9ce51f260bacc012fe90ae615722be8082d4
andicoaker <Andi@Andreas-MacBook.local>
2017-01-31 22:47:25 -0500

    solved ex-01, starting ex-02

--- ex-01-getLast.js
+++ ex-01-getLast.js
@@ -1,11 +1,19 @@
-/** 
+/**
  * Ex-01: getLast()
  *
  * Write a function called getLast() that returns the last element of an array.
  *
 **/
+//
+// INPUT: array
+// OUTPUT: last element of array
 
+var getLast = function (stuffArr){
+  var lastElement = stuffArr[stuffArr.length - 1]
+  // console.log(lastElement);
 
+  return lastElement
+}
 
 console.assert( getLast([1,8,4,3]) === 3 )
 console.assert( getLast([1,8,4,3,5]) === 5 )
